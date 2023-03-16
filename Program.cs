89ed1ed7ed509c63150dbf16dc810fad906b719b
Juju <Juju@DESKTOP-ID1UP5M>
2023-03-16 18:52:47 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace DiamanteLetras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite uma letra: ");
+            string letra = (Console.ReadLine()).ToUpper();
+
+            string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] alfabetoArray = alfabeto.ToCharArray();
+
+
+            int indexLetra = alfabeto.IndexOf(letra);
+
+
+            for (int i = 0; i <= indexLetra; i++)
+            {
+                Console.WriteLine("\n");
+                for (int k = 0; k < indexLetra - i; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int h = 0; h < 1; h++)
+                {
+                    Console.Write(alfabetoArray[i]);
+
+                    for (int k = 0; k <= 2 * i - 2; k++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    if (i != 0)
+                    {
+                        Console.WriteLine(alfabetoArray[i]);
+                    }
+                }
+
+            }
+
+            for (int i = indexLetra - 1; i >= 0; i--)
+            {
+                Console.WriteLine("\n");
+                for (int k = 0; k < indexLetra - i; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int h = 0; h < 1; h++)
+                {
+                    Console.Write(alfabetoArray[i]);
+
+                    for (int k = 0; k <= 2 * i - 2; k++)
+                    {
+                        Console.Write(" ");
+                    }
+                    if (i != 0)
+                    {
+                        Console.WriteLine(alfabetoArray[i]);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
